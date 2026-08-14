741937ba0cafbc85df32d489d6356f7b3a80f00a
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-08-14 17:32:53 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace guirtf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName.Trim() != "") 
+            {
+                 richTextBox1.LoadFile(openFileDialog1.FileName.Trim());
+
+            
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName.Trim() != "") 
+            {
+                 richTextBox1.SaveFile(saveFileDialog1.FileName.Trim());
+
+            
+            
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName.Trim() != "")
+            {
+                richTextBox1.SaveFile(saveFileDialog1.FileName.Trim());
+
+
+
+            }
+            richTextBox1.Clear();
+
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName.Trim() != "")
+            {
+                richTextBox1.SaveFile(saveFileDialog1.FileName.Trim());
+
+
+
+            }
+            richTextBox1.Clear();
+            
+        }
+    }
+}
